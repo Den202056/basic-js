0f2a2d20f08312f93ff529ced5facc394d532649
Den202056 <jsworkmail19@gmail.com>
2022-10-18 02:34:39 +0500

    feat: add partial solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,60 +5,82 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 
-const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
-
 // const chainMaker = {
-//   len: 0,
-//   chain: [],
-
 //   getLength() {
-//     return this.len;
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
 //   },
-
-//   addLink(value) {
-//     this.chain.push(value);
-//     this.len += 1;
+//   addLink(/* value */) {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
 //   },
-
 //   removeLink(/* position */) {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
 //   },
-
 //   reverseChain() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
 //   },
-
 //   finishChain() {
-//     let str = '';
-//     this.chain.forEach((item, index) => {
-//       str += '(' + item + ')';
-//       console.log('str: ', str);
-//       if (!(index === this.len - 1)) str += '~~';
-//     });
-//     return str;
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
 //   }
+// };
 
-// }
+const chainMaker = {
 
+  len: 0,
+  chain: [],
+
+  getLength() {
+    return this.len;
+  },
+
+  addLink(value) {
+    if (arguments[0] === undefined) {
+      this.chain.push('');
+      this.len += 1;
+      return this;
+    } else {
+      this.chain.push(String(value));
+      this.len += 1;
+      return this;
+    }
+  },
+
+  removeLink(position) {
+    if (typeof position !== 'number') {
+      throw new Error('You can\'t remove incorrect link!');
+    } else if (position <= 0 || position > this.len) {
+      throw new Error('You can\'t remove incorrect link!');
+    } else {
+      this.chain.splice(position - 1, 1);
+      this.len -= 1;
+      return this;
+    }
+  },
+
+  reverseChain() {
+    this.chain.reverse();
+    return this;
+  },
+
+  finishChain() {
+    let str = '';
+    const currentChain = this.chain;
+    const currentLen = this.len;
+
+    currentChain.forEach((item, index) => {
+      str += '( ' + item + ' )';
+      if (!(index === currentLen - 1)) str += '~~';
+    });
+    this.chain = [];
+    this.len = 0;
+    return str;
+  }
+
+}
 
 module.exports = {
   chainMaker
